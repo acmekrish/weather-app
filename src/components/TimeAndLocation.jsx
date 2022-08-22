2c8647abb21fd37df6da95a8d351275e563afd5d
Krish Acme <acmekrish@gmail.com>
2022-08-22 22:39:05 +0100

    last-commit

--- src/components/TimeAndLocation.jsx
+++ src/components/TimeAndLocation.jsx
@@ -1,20 +1,20 @@
-import React from 'react'
+import React from "react";
+import { formatToLocalTime } from "../services/weatherService";
 
-function TimeAndLocation() {
+function TimeAndLocation({ weather: { dt, timezone, name, country } }) {
   return (
     <div>
-      <div className = "flex item-center justify-center my-6">
+      <div className="flex items-center justify-center my-6">
         <p className="text-white text-xl font-extralight">
-            Saturday, 19 August 2022 | Local time: 8:31 PM
+          {formatToLocalTime(dt, timezone)}
         </p>
       </div>
-      <div className = "flex item-center justify-center my-3">
-        <p className="text-white text-3xl font-medium">
-            Nigeria, NG
-        </p>
+
+      <div className="flex items-center justify-center my-3">
+        <p className="text-white text-3xl font-medium">{`${name}, ${country}`}</p>
       </div>
     </div>
-  )
+  );
 }
 
-export default TimeAndLocation
+export default TimeAndLocation;
